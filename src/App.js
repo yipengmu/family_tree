c5e36ba08c2a7337664ce7a2fef64fb64e7806f3
luoyuan <luoyuan.myp@alibaba-inc.com>
2018-10-17 19:31:49 +0800

    数据从服务端获取

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TreeList from './components/treeList/index.js'
+
+import 'whatwg-fetch'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>family tree</p>
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: {},
+        };
+    }
 
-        </header>
-      </div>
-    );
-  }
+    componentDidMount() {
+        fetch('http://localhost:7002/api/ft/queryTree')
+            .then((response)=> {
+                return response.json()
+            }).then((json)=> {
+                this.setState({result:json.result});
+                console.log('parsed json', json.result);
+            }).catch((ex)=> {
+                console.log('parsing failed', ex);
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p>family tree header</p>
+
+                <div className="App-container">
+                    <TreeList result={this.state.result}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
