b7c27a2bd979122671a2fae4927a88e21703eea8
luoyuan <luoyuan.myp@alibaba-inc.com>
2018-10-17 22:32:19 +0800

    主要页面内容展示

--- src/components/treeList/index.js
+++ src/components/treeList/index.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import styles from './index.css';
+import './index.css';
 import { List } from 'antd';
 import 'whatwg-fetch'
 
@@ -10,14 +10,27 @@ class TreeList extends Component {
 
         console.log('## render json', JSON.stringify(result));
         return (
-            <div className={styles['tree-list']}>
+            <div >
 
                 <List
                     itemLayout="horizontal"
+                    locale='加载中'
                     dataSource={result}
+                    footer={<div>Footer</div>}
+                    bordered={true}
+                    split={true}
                     renderItem={item => (
-                      <List.Item>
-                      {item.name}
+                      <List.Item className='tree-list' >
+                        <span className='list-item-small' style={{width: '50px'}} >{'id：'+ item.id}</span >
+                        <span className='list-item-small' >{'第：'+ item.g_rank +'代'}</span >
+                        <span className='list-item-normal' >{'姓名：'+ item.name}</span >
+                        <span className='list-item-small' >{'父id：'+ item.g_father_id}</span >
+                        <span className='list-item-small' >{'家中排行：'+ item.rank_index}</span >
+                        <span className='list-item' >{'过继关系：'+ item.adoption}</span >
+                        <span className='list-item' style={{width: '140px'}}>{'功名：'+ item.official_position}</span >
+                        <span className='list-item' >{'简介：'+ item.summary}</span >
+
+
                       </List.Item>
                     )}
                     />
