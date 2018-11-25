bb63f52c2431f86d8eb5cdf198fe74715b5442ab
luoyuan <luoyuan.myp@alibaba-inc.com>
2018-11-26 00:11:18 +0800

    json transfer

--- src/App.js
+++ src/App.js
@@ -23,9 +23,17 @@ class App extends Component {
                 //console.log('原始数据 parsed json', JSON.stringify(json.result));
                 //let fatherTree = util.getFatherTree(545,json.result,[]);
 
-                let fatherTree = util.getFatherTrees(json.result);
-                console.log('fatherTrees 输出 ', JSON.stringify(fatherTree));
-                //console.log('json build = '+ JSON.stringify(util.getJsonFromDBResult(0,{},json.result)));
+                let fatherTrees = util.getFatherTrees(json.result);
+                console.log('fatherTrees 输出： ', fatherTrees);
+
+                let transferFatherTrees = util.transferFatherTrees(fatherTrees);
+                console.log('transferFatherTrees 输出： ', transferFatherTrees);
+
+                //let fatherTreesJson = util.getFatherTreesJson(fatherTrees);
+                console.log('fatherTrees 数组长度为： ', fatherTrees.length);
+                //console.log('fatherTrees 输出 ', JSON.stringify(fatherTrees));
+                //console.log('fatherTreesJson 输出 ', fatherTreesJson);
+
             }).catch((ex)=> {
                 console.log('parsing failed', ex);
             })
