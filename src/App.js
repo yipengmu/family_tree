87d2e10cd13ff72f4e868807b8f5312694c426c4
luoyuan <luoyuan.myp@alibaba-inc.com>
2019-01-01 16:06:34 +0800

    合并setState

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ class App extends Component {
             .then((response)=> {
                 return response.json()
             }).then((json)=> {
-                this.setState({resultArray:json.result});
-                //console.log('原始数据 parsed json', JSON.stringify(json.result));
-                //let fatherTree = util.getFatherTree(545,json.result,[]);
+
 
                 // 将数据库数据转为二维数组数据
                 let fatherTrees = util.getFatherTrees(json.result);
@@ -32,26 +30,14 @@ class App extends Component {
                 let reductiveTrees = util.reductFatherTrees(fatherTrees);
                 //console.log('reductiveTrees 输出： ', reductiveTrees);
                 //console.log('myp 输出： ', JSON.stringify(reductiveTrees[617]));
-                //console.log('600 输出： ', JSON.stringify(reductiveTrees[600]));
 
                 // 将622条深度直系父关系，借助g_rank进行合并
                 let oneTree = util.getOneTree(reductiveTrees);
                 console.log('oneTree 输出： ', oneTree);
 
-                this.setState({resultOneTree: oneTree});
+                this.setState({resultArray:json.result,resultOneTree: oneTree});
+
                 //console.log('oneTree 输出： ', JSON.stringify(oneTree));
-
-
-                //  将二维的各id的父节点数组组合 转为为 1维的 各id的父节点json对象组合
-                //let transferFatherTrees = util.transferFatherTrees(fatherTrees);
-                //console.log('transferFatherTrees 输出： ', transferFatherTrees);
-                //console.log('transferFatherTrees 输出： ', JSON.stringify(transferFatherTrees));
-
-                //let fatherTreesJson = util.getFatherTreesJson(fatherTrees);
-                //console.log('fatherTrees 数组长度为： ', fatherTrees.length);
-                //console.log('fatherTrees 输出 ', JSON.stringify(fatherTrees));
-                //console.log('fatherTreesJson 输出 ', fatherTreesJson);
-
             }).catch((ex)=> {
                 console.log('parsing failed', ex);
             })
